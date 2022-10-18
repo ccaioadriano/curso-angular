35365a96f3590e3fe06ba8be0d2e7296bceff7de
unknown <caiobhadriano@gmail.com>
2022-10-18 00:09:24 -0300

    Injeção de dependencias e introdução aos services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { CursosModule } from './cursos/cursos.module';
-import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MeuPrimeiroComponent,
   ],
   imports: [
     BrowserModule,
